137741fb2758bc412b2dfc90111070b2d99abee6
Zou Jian <chsword@126.com>
2018-01-10 11:06:53 +0800

    add sp message folder

---
+++ src/STB.Core/Structures/SpendProofs.cs
@@ -0,0 +1,19 @@
+﻿/*---------------------------------------------------------------------------------------------
+ * Copyright (c) STB Chain. All rights reserved.
+ * Licensed under the Source EULA. See License in the project root for license information.
+ * Source code : https://github.com/stbchain
+ * Website : http://www.soft2b.com/
+ *---------------------------------------------------------------------------------------------
+ ---------------------------------------------------------------------------------------------*/
+namespace STB.Core.Structures
+{
+    public class SpendProofs
+    {
+        public Unit Unit { get; set; }
+        public int MessageIndex { get; set; }
+        public byte SpendProofIndex { get; set; }
+        public string SpendProof { get; set; }
+        public string Address { get; set; }
+
+    }
+}
