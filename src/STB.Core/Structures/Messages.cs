3167bb6dc130554f38451c078dc4587e3fbceeed
Zou Jian <chsword@126.com>
2018-01-10 10:59:42 +0800

    Create Message class

---
+++ src/STB.Core/Structures/Messages.cs
@@ -0,0 +1,24 @@
+﻿/*---------------------------------------------------------------------------------------------
+ * Copyright (c) STB Chain. All rights reserved.
+ * Licensed under the Source EULA. See License in the project root for license information.
+ * Source code : https://github.com/stbchain
+ * Website : http://www.soft2b.com/
+ *---------------------------------------------------------------------------------------------
+ ---------------------------------------------------------------------------------------------*/
+namespace STB.Core.Structures
+{
+    public class Messages
+    {
+        public Unit Unit { get; set; }
+        public int MessageIndex { get; set; }
+
+        public int App { get; set; }
+
+        public string PayloadLocation { get; set; }
+        public string PayloadHash { get; set; }
+        public string Payload { get; set; }
+        public string PayloadUriHash { get; set; }
+        public string PayloadUri { get; set; }
+        
+    }
+}
