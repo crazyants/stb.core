f1286228ed80fd0af0125a0bcd28e2315c1442e9
Zou Jian <chsword@126.com>
2018-01-20 16:53:49 +0800

    Format HashUtil

--- src/STB.Core/HashUtil.cs
+++ src/STB.Core/HashUtil.cs
@@ -15,20 +15,21 @@ namespace STB.Core
 {
     public class HashUtil
     {
-        static string PI = "14159265358979323846264338327950288419716939937510";
-        static string zeroString = "00000000";
+        private static readonly string PI = "14159265358979323846264338327950288419716939937510";
 
-        static char[] arrRelativeOffsets = PI.ToCharArray();
-        static List<int> arrOffsets160 = CalcOffsets(160);
-        static List<int> arrOffsets288 = CalcOffsets(288);
 
-        static void CheckLength(int hashLength)
+        private static readonly char[] ArrRelativeOffsets = PI.ToCharArray();
+        private static readonly List<int> ArrOffsets160 = CalcOffsets(160);
+        private static readonly List<int> ArrOffsets288 = CalcOffsets(288);
+
+        // ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
+        private static void CheckLength(int hashLength)
         {
             if (hashLength != 160 && hashLength != 288)
-                throw new System.Exception("hashLength error");
+                throw new Exception("hashLength error");
         }
 
-        static List<int> CalcOffsets(int hashLength)
+        private static List<int> CalcOffsets(int hashLength)
         {
             CheckLength(hashLength);
             var arrOffsets = new List<int>();
@@ -37,7 +38,7 @@ namespace STB.Core
 
             for (var i = 0; offset < hashLength; i++)
             {
-                var relativeOffset = (int) (arrRelativeOffsets[i]);
+                var relativeOffset = (int) ArrRelativeOffsets[i];
                 if (relativeOffset == 0)
                     continue;
                 offset += relativeOffset;
@@ -50,30 +51,30 @@ namespace STB.Core
             }
 
             if (index != 32)
-                throw new System.Exception("wrong number of checksum bits");
+                throw new Exception("wrong number of checksum bits");
 
             return arrOffsets;
         }
 
-        static (string, string) separateIntoCleanDataAndChecksum(string bin)
+        private static (string, string) SeparateIntoCleanDataAndChecksum(string bin)
         {
             var len = bin.Length;
             List<int> arrOffsets;
             if (len == 160)
-                arrOffsets = arrOffsets160;
+                arrOffsets = ArrOffsets160;
             else if (len == 288)
-                arrOffsets = arrOffsets288;
+                arrOffsets = ArrOffsets288;
             else
-                throw new System.Exception("bad length=" + len + ", bin = " + bin);
+                throw new Exception("bad length=" + len + ", bin = " + bin);
             var arrFrags = new List<string>();
             var arrChecksumBits = new List<string>();
             var start = 0;
-            for (var i = 0; i < arrOffsets.Count; i++)
+            foreach (var t in arrOffsets)
             {
-                arrFrags.Add(bin.Substring(start, arrOffsets[i]));
+                arrFrags.Add(bin.Substring(start, t));
 
-                arrChecksumBits.Add(bin.Substring(arrOffsets[i], 1)); //todo Substr
-                start = arrOffsets[i] + 1;
+                arrChecksumBits.Add(bin.Substring(t, 1)); //todo Substr
+                start = t + 1;
             }
 
             // add last frag
@@ -84,19 +85,19 @@ namespace STB.Core
             return (binCleanData, binChecksum);
         }
 
-        string mixChecksumIntoCleanData(string binCleanData, string binChecksum)
+        private string mixChecksumIntoCleanData(string binCleanData, string binChecksum)
         {
             if (binChecksum.Length != 32)
-                throw new System.Exception("bad checksum length");
+                throw new Exception("bad checksum length");
             var len = binCleanData.Length + binChecksum.Length;
             List<int> arrOffsets;
             if (len == 160)
-                arrOffsets = arrOffsets160;
+                arrOffsets = ArrOffsets160;
             else if (len == 288)
-                arrOffsets = arrOffsets288;
+                arrOffsets = ArrOffsets288;
             else
-                throw new System.Exception("bad length=" + len + ", clean data = " + binCleanData + ", checksum = " +
-                                           binChecksum);
+                throw new Exception("bad length=" + len + ", clean data = " + binCleanData + ", checksum = " +
+                                    binChecksum);
             var arrFrags = new List<string>();
             var arrChecksumBits = binChecksum.ToCharArray();
             var start = 0;
@@ -114,7 +115,7 @@ namespace STB.Core
             return string.Join("", arrFrags);
         }
 
-        string buffer2bin(byte[] buf)
+        private string buffer2bin(byte[] buf)
         {
             var bytes = new List<string>();
             foreach (var number in buf)
@@ -126,7 +127,7 @@ namespace STB.Core
             return string.Join("", bytes);
         }
 
-        byte[] bin2buffer(string bin)
+        private byte[] bin2buffer(string bin)
         {
             var len = bin.Length / 8;
             var buf = new byte[len];
@@ -135,7 +136,7 @@ namespace STB.Core
             return buf;
         }
 
-        byte[] getChecksum(byte[] cleanData)
+        private byte[] getChecksum(byte[] cleanData)
         {
             byte[] fullChecksum;
             using (var sha1 = SHA256.Create())
@@ -147,25 +148,25 @@ namespace STB.Core
             return checksum;
         }
 
-        string getChash(string data, int chash_length)
+        private string GetChash(string data, int chashLength)
         {
-            CheckLength(chash_length);
+            CheckLength(chashLength);
             var dataBytes = Encoding.UTF8.GetBytes(data);
-            var hash = createHash((chash_length == 160) ? "ripemd160" : "sha256", dataBytes);
+            var hash = createHash(chashLength == 160 ? "ripemd160" : "sha256", dataBytes);
             //console.log("hash", hash);
-            var truncated_hash = (chash_length == 160) ? hash.Slice(4) : hash; // drop first 4 bytes if 160
+            var truncatedHash = chashLength == 160 ? hash.Slice(4) : hash; // drop first 4 bytes if 160
             //console.log("clean data", truncated_hash);
-            var checksum = getChecksum(truncated_hash);
+            var checksum = getChecksum(truncatedHash);
             //console.log("checksum", checksum);
             //console.log("checksum", buffer2bin(checksum));
 
-            var binCleanData = buffer2bin(truncated_hash);
+            var binCleanData = buffer2bin(truncatedHash);
             var binChecksum = buffer2bin(checksum);
             var binChash = mixChecksumIntoCleanData(binCleanData, binChecksum);
             //console.log(binCleanData.length, binChecksum.length, binChash.length);
             var chash = bin2buffer(binChash);
             //console.log("chash     ", chash);
-            var encoded = (chash_length == 160) ? Base32.Encode(chash) : Convert.ToBase64String(chash);
+            var encoded = chashLength == 160 ? Base32.Encode(chash) : Convert.ToBase64String(chash);
             //console.log(encoded);
 
             return encoded;
@@ -173,31 +174,31 @@ namespace STB.Core
 
         public string getChash160(string data)
         {
-            return getChash(data, 160);
+            return GetChash(data, 160);
         }
 
         public string getChash288(string data)
         {
-            return getChash(data, 288);
+            return GetChash(data, 288);
         }
 
         public bool isChashValid(string encoded)
         {
-            var encoded_len = encoded.Length;
-            if (encoded_len != 32 && encoded_len != 48) // 160/5 = 32, 288/6 = 48
-                throw new Exception("wrong encoded length: " + encoded_len);
+            var encodedLen = encoded.Length;
+            if (encodedLen != 32 && encodedLen != 48) // 160/5 = 32, 288/6 = 48
+                throw new Exception("wrong encoded length: " + encodedLen);
             try
             {
-                var chash = (encoded_len == 32) ? Base32.Decode(encoded) : Convert.FromBase64String(encoded);
+                var chash = encodedLen == 32 ? Base32.Decode(encoded) : Convert.FromBase64String(encoded);
 
                 var binChash = buffer2bin(chash);
-                var separated = separateIntoCleanDataAndChecksum(binChash);
-                var clean_data = bin2buffer(separated.Item1);
+                var separated = SeparateIntoCleanDataAndChecksum(binChash);
+                var cleanData = bin2buffer(separated.Item1);
                 //console.log("clean data", clean_data);
                 var checksum = bin2buffer(separated.Item2);
                 //console.log(checksum);
                 //console.log(getChecksum(clean_data));
-                return checksum.Equals(getChecksum(clean_data));
+                return checksum.Equals(getChecksum(cleanData));
             }
             catch (Exception e)
             {
@@ -205,23 +206,19 @@ namespace STB.Core
             }
         }
 
-        byte[] createHash(string hashAlgorithm, byte[] data)
+        private byte[] createHash(string hashAlgorithm, byte[] data)
         {
             if (hashAlgorithm == "sha256")
-            {
                 using (var sha1 = SHA256.Create())
                 {
                     return sha1.ComputeHash(data);
                 }
-            }
 
             if (hashAlgorithm == "ripemd160")
-            {
                 using (var ripemd160 = RIPEMD160.Create())
                 {
                     return ripemd160.ComputeHash(data);
                 }
-            }
 
             return null;
         }
